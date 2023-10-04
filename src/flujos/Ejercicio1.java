6f9c038663ed4ddd684370fe541682e662a718c9
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-04 12:52:14 +0200

    Ejercicio1 hecho

---
+++ src/flujos/Ejercicio1.java
@@ -0,0 +1,23 @@
+package flujos;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombreFich = "";
+        for (String arg: args) {
+            nombreFich += arg;
+        }
+        File fich = new File(nombreFich);
+        try (FileReader fr = new FileReader(fich)) {
+            int i;
+            while ((i = fr.read()) != -1){
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

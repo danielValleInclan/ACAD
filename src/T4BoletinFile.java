7f7acf28f9ba7230d6e5c662aea933bae2278733
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-04 10:31:37 +0200

    Mejorado T4BoletinFile

--- src/T4BoletinFile.java
+++ src/T4BoletinFile.java
@@ -3,23 +3,19 @@ import java.io.File;
 public class T4BoletinFile {
     public static void main(String[] args) {
         File listador = new File("/home/daniel/Escritorio/");
-        listaDirectorios(listador, 0);
+        listaDirectorios(listador, "*");
     }
-    public static boolean listaDirectorios(File file, int cont){
-        cont++;
-        String[] listado = file.list();
+    public static boolean listaDirectorios(File file, String separador){
+        File[] listado = file.listFiles();
         if (listado.length == 0){
             System.out.println("No hay más elementos");
             return true;
         }   else {
-            for (String s : listado) {
-                for (int i = 0; i < cont; i++) {
-                    System.out.print("*");
-                }
-                System.out.println(s);
-                File auxfile = new File(file, s);
-                if (auxfile.isDirectory()) {
-                    listaDirectorios(auxfile, cont);
+            for (File s : listado) {
+                separador += "*";
+                System.out.println(separador + s.getName());
+                if (s.isDirectory()) {
+                    listaDirectorios(s, separador);
                 }
             }
             return true;

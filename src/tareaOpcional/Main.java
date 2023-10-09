065b4a7e9dde5bf3586216549768b5df285d8a3f
danieldrv8 <danieldrv8@gmail.com>
2023-10-09 20:29:06 +0200

    Terminada tarea opcional.

--- src/tareaOpcional/Main.java
+++ src/tareaOpcional/Main.java
@@ -1,6 +1,7 @@
 package tareaOpcional;
 
 import java.io.File;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,16 @@ public class Main {
         try {
             diffFolder.setFolders(prueba1, prueba2);
         } catch (GestionFicherosExeption exeption){
-            System.out.println(exeption);
+            System.err.println("Error: " + exeption.getMessage());
+            return;
+        }
+        // Realizar la comparación y obtener el iterador de resultados
+        Iterator<ResultadoComparacion> resultados = diffFolder.compare();
+
+        // Iterar a través de los resultados e imprimirlos
+        while (resultados.hasNext()) {
+            ResultadoComparacion resultado = resultados.next();
+            System.out.println("Nombre: " + resultado.getNomFichero() + ", Comparación: " + resultado.getValorComparacion());
         }
     }
 }

52a3b19161ded7338e4d43a79987cd2f6d297523
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-18 12:55:22 +0200

    No termniando Ejericio 9 de Tarea 4

--- src/boletin3/ejercicio8/Contacto.java
+++ src/boletin3/ejercicio8/Contacto.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 
 public class Contacto implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
     private String nombre, apellidos, telefono;
 
     public Contacto(String nombre, String apellidos, String telefono) {
